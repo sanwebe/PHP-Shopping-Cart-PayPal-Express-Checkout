bac2b5bfcf1547b4b857899fa1210bad87f81038
Sanwebe <saaraan@gmail.com>
2016-01-15 14:24:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 include_once("config.php");
+
+
+//current URL of the Page. cart_update.php redirects back to this URL
+$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,71 +13,100 @@ include_once("config.php");
 <title>Shopping Cart</title>
 <link href="style/style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-<div id="products-wrapper">
-    <h1>Products</h1>
-    <div class="products">
-    <?php
-    //current URL of the Page. cart_update.php redirects back to this URL
-	$current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-    
-	$results = $mysqli->query("SELECT * FROM products ORDER BY id ASC");
-    if ($results) { 
-	
-        //fetch results set as object and output HTML
-        while($obj = $results->fetch_object())
-        {
-			echo '<div class="product">'; 
-            echo '<form method="post" action="cart_update.php">';
-			echo '<div class="product-thumb"><img src="images/'.$obj->product_img_name.'"></div>';
-            echo '<div class="product-content"><h3>'.$obj->product_name.'</h3>';
-            echo '<div class="product-desc">'.$obj->product_desc.'</div>';
-            echo '<div class="product-info">';
-			echo 'Price '.$currency.$obj->price.' | ';
-            echo 'Qty <input type="text" name="product_qty" value="1" size="3" />';
-			echo '<button class="add_to_cart">Add To Cart</button>';
-			echo '</div></div>';
-            echo '<input type="hidden" name="product_code" value="'.$obj->product_code.'" />';
-            echo '<input type="hidden" name="type" value="add" />';
-			echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
-            echo '</form>';
-            echo '</div>';
-        }
-    
-    }
-    ?>
-    </div>
-    
-<div class="shopping-cart">
-<h2>Your Shopping Cart</h2>
+
+<h1 align="center">Products </h1>
+
+<!-- View Cart Box Start -->
 <?php
-if(isset($_SESSION["products"]))
+if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
 {
-    $total = 0;
-    echo '<ol>';
-    foreach ($_SESSION["products"] as $cart_itm)
-    {
-        echo '<li class="cart-itm">';
-        echo '<span class="remove-itm"><a href="cart_update.php?removep='.$cart_itm["code"].'&return_url='.$current_url.'">&times;</a></span>';
-        echo '<h3>'.$cart_itm["name"].'</h3>';
-        echo '<div class="p-code">P code : '.$cart_itm["code"].'</div>';
-        echo '<div class="p-qty">Qty : '.$cart_itm["qty"].'</div>';
-        echo '<div class="p-price">Price :'.$currency.$cart_itm["price"].'</div>';
-        echo '</li>';
-        $subtotal = ($cart_itm["price"]*$cart_itm["qty"]);
-        $total = ($total + $subtotal);
-    }
-    echo '</ol>';
-    echo '<span class="check-out-txt"><strong>Total : '.$currency.$total.'</strong> <a href="view_cart.php">Check-out!</a></span>';
-	echo '<span class="empty-cart"><a href="cart_update.php?emptycart=1&return_url='.$current_url.'">Empty Cart</a></span>';
-}else{
-    echo 'Your Cart is empty';
+	echo '<div class="cart-view-table-front" id="view-cart">';
+	echo '<h3>Your Shopping Cart</h3>';
+	echo '<form method="post" action="cart_update.php">';
+	echo '<table width="100%"  cellpadding="6" cellspacing="0">';
+	echo '<tbody>';
+
+	$total =0;
+	$b = 0;
+	foreach ($_SESSION["cart_products"] as $cart_itm)
+	{
+		$product_name = $cart_itm["product_name"];
+		$product_qty = $cart_itm["product_qty"];
+		$product_price = $cart_itm["product_price"];
+		$product_code = $cart_itm["product_code"];
+		$product_color = $cart_itm["product_color"];
+		$bg_color = ($b++%2==1) ? 'odd' : 'even'; //zebra stripe
+		echo '<tr class="'.$bg_color.'">';
+		echo '<td>Qty <input type="text" size="2" maxlength="2" name="product_qty['.$product_code.']" value="'.$product_qty.'" /></td>';
+		echo '<td>'.$product_name.'</td>';
+		echo '<td><input type="checkbox" name="remove_code[]" value="'.$product_code.'" /> Remove</td>';
+		echo '</tr>';
+		$subtotal = ($product_price * $product_qty);
+		$total = ($total + $subtotal);
+	}
+	echo '<td colspan="4">';
+	echo '<button type="submit">Update</button><a href="view_cart.php" class="button">Checkout</a>';
+	echo '</td>';
+	echo '</tbody>';
+	echo '</table>';
+	
+	$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+	echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
+	echo '</form>';
+	echo '</div>';
+
 }
 ?>
-</div>
-    
-</div>
+<!-- View Cart Box End -->
 
+
+<!-- Products List Start -->
+<?php
+$results = $mysqli->query("SELECT product_code, product_name, product_desc, product_img_name, price FROM products ORDER BY id ASC");
+if($results){ 
+$products_item = '<ul class="products">';
+//fetch results set as object and output HTML
+while($obj = $results->fetch_object())
+{
+$products_item .= <<<EOT
+	<li class="product">
+	<form method="post" action="cart_update.php">
+	<div class="product-content"><h3>{$obj->product_name}</h3>
+	<div class="product-thumb"><img src="images/{$obj->product_img_name}"></div>
+	<div class="product-desc">{$obj->product_desc}</div>
+	<div class="product-info">
+	Price {$currency}{$obj->price} 
+	
+	<fieldset>
+	
+	<label>
+		<span>Color</span>
+		<select name="product_color">
+		<option value="Black">Black</option>
+		<option value="Silver">Silver</option>
+		</select>
+	</label>
+	
+	<label>
+		<span>Quantity</span>
+		<input type="text" size="2" maxlength="2" name="product_qty" value="1" />
+	</label>
+	
+	</fieldset>
+	<input type="hidden" name="product_code" value="{$obj->product_code}" />
+	<input type="hidden" name="type" value="add" />
+	<input type="hidden" name="return_url" value="{$current_url}" />
+	<div align="center"><button type="submit" class="add_to_cart">Add</button></div>
+	</div></div>
+	</form>
+	</li>
+EOT;
+}
+$products_item .= '</ul>';
+echo $products_item;
+}
+?>    
+<!-- Products List End -->
 </body>
 </html>
