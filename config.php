e3c82e6709feb33e6d72f06d35935a02afb315d4
Sanwebe <saaraan@gmail.com>
2016-01-15 14:24:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,34 @@
 <?php
-$currency = '$'; //Currency sumbol or code
+$currency = '$'; //Currency Character or code
 
-//db settings
-$db_username = 'root';
-$db_password = '';
-$db_name = 'demo';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+//MySql 
+$db_username 	= 'root';
+$db_password 	= '';
+$db_name 		= 'test';
+$db_host 		= 'localhost';
 
 //paypal settings
 $PayPalMode 			= 'sandbox'; // sandbox or live
-$PayPalApiUsername 		= 'paypaluser@somemail.com'; //PayPal API Username
-$PayPalApiPassword 		= '190798792445'; //Paypal API password
-$PayPalApiSignature 	= '797987709709709oipuiou98Eq.Gufar'; //Paypal API Signature
+$PayPalApiUsername 		= 'account@gmail.com'; //PayPal API Username
+$PayPalApiPassword 		= '979797979'; //Paypal API password
+$PayPalApiSignature 	= 'AewouidSeoiewDradoZcgqH3hpacAokIiuNjAwoiedkew'; //Paypal API Signature
 $PayPalCurrencyCode 	= 'USD'; //Paypal Currency Code
-$PayPalReturnURL 		= 'http://localhost/shopping-cart/paypal-express-checkout/process.php'; //Point to process.php page
-$PayPalCancelURL 		= 'http://localhost/shopping-cart/paypal-express-checkout/cancel_url.html'; //Cancel URL if user clicks cancel
+$PayPalReturnURL 		= 'http://yoursite.com/php-shopping-cart-master/paypal-express-checkout/'; //Point to paypal-express-checkout page
+$PayPalCancelURL 		= 'http://yoursite.com/shopping-cart/paypal-express-checkout/cancel_url.html'; //Cancel URL if user clicks cancel
 
+//Additional taxes and fees											
+$HandalingCost 		= 0.00;  //Handling cost for the order.
+$InsuranceCost 		= 0.00;  //shipping insurance cost for the order.
+$shipping_cost      = 1.50; //shipping cost
+$ShippinDiscount 	= 0.00; //Shipping discount for this order. Specify this as negative number (eg -1.00)
+$taxes              = array( //List your Taxes percent here.
+                            'VAT' => 12, 
+                            'Service Tax' => 5
+                            );
+
+//connection to MySql						
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);						
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
 ?>
