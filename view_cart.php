9fc53cef599a10f0779966f8031da546e03b8d1a
Sanwebe <saaraan@gmail.com>
2016-01-15 14:26:19 +0530

    Update view_cart.php

--- view_cart.php
+++ view_cart.php
@@ -6,58 +6,78 @@ include_once("config.php");
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>View shopping cart</title>
 <link href="style/style.css" rel="stylesheet" type="text/css"></head>
 <body>
-<div id="products-wrapper">
- <h1>View Cart</h1>
- <div class="view-cart">
+<h1 align="center">View Cart</h1>
+<div class="cart-view-table-back">
+<form method="post" action="cart_update.php">
+<table width="100%"  cellpadding="6" cellspacing="0">
+    <thead>
+    	<tr><th>Quantity</th><th>Name</th><th>Price</th><th>Total</th><th>Remove</th></tr>
+    </thead>
+  <tbody>
  	<?php
-    $current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-	if(isset($_SESSION["products"]))
+	if(isset($_SESSION["cart_products"])) //check session var
     {
-	    $total = 0;
-		echo '<form method="post" action="paypal-express-checkout/process.php">';
-		echo '<ul>';
-		$cart_items = 0;
-		foreach ($_SESSION["products"] as $cart_itm)
+		$total = 0; //set initial total value
+		$b = 0; //var for zebra stripe table 
+		foreach ($_SESSION["cart_products"] as $cart_itm)
         {
-           $product_code = $cart_itm["code"];
-		   $results = $mysqli->query("SELECT product_name,product_desc, price FROM products WHERE product_code='$product_code' LIMIT 1");
-		   $obj = $results->fetch_object();
-		   
-		    echo '<li class="cart-itm">';
-			echo '<span class="remove-itm"><a href="cart_update.php?removep='.$cart_itm["code"].'&return_url='.$current_url.'">&times;</a></span>';
-			echo '<div class="p-price">'.$currency.$obj->price.'</div>';
-            echo '<div class="product-info">';
-			echo '<h3>'.$obj->product_name.' (Code :'.$product_code.')</h3> ';
-            echo '<div class="p-qty">Qty : '.$cart_itm["qty"].'</div>';
-            echo '<div>'.$obj->product_desc.'</div>';
-			echo '</div>';
-            echo '</li>';
-			$subtotal = ($cart_itm["price"]*$cart_itm["qty"]);
-			$total = ($total + $subtotal);
-
-			echo '<input type="hidden" name="item_name['.$cart_items.']" value="'.$obj->product_name.'" />';
-			echo '<input type="hidden" name="item_code['.$cart_items.']" value="'.$product_code.'" />';
-			echo '<input type="hidden" name="item_desc['.$cart_items.']" value="'.$obj->product_desc.'" />';
-			echo '<input type="hidden" name="item_qty['.$cart_items.']" value="'.$cart_itm["qty"].'" />';
-			$cart_items ++;
+			//set variables to use in content below
+			$product_name = $cart_itm["product_name"];
+			$product_qty = $cart_itm["product_qty"];
+			$product_price = $cart_itm["product_price"];
+			$product_code = $cart_itm["product_code"];
+			$product_color = $cart_itm["product_color"];
+			$subtotal = ($product_price * $product_qty); //calculate Price x Qty
 			
+		   	$bg_color = ($b++%2==1) ? 'odd' : 'even'; //class for zebra stripe 
+		    echo '<tr class="'.$bg_color.'">';
+			echo '<td><input type="text" size="2" maxlength="2" name="product_qty['.$product_code.']" value="'.$product_qty.'" /></td>';
+			echo '<td>'.$product_name.'</td>';
+			echo '<td>'.$currency.$product_price.'</td>';
+			echo '<td>'.$currency.$subtotal.'</td>';
+			echo '<td><input type="checkbox" name="remove_code[]" value="'.$product_code.'" /></td>';
+            echo '</tr>';
+			$total = ($total + $subtotal); //add subtotal to total var
         }
-    	echo '</ul>';
-		echo '<span class="check-out-txt">';
-		echo '<strong>Total : '.$currency.$total.'</strong>  ';
-		echo '<input type="submit" value="Pay Now" />';
-		echo '</span>';
-		echo '</form>';
 		
-    }else{
-		echo 'Your Cart is empty';
+		$grand_total = $total + $shipping_cost; //grand total including shipping cost
+		foreach($taxes as $key => $value){ //list and calculate all taxes in array
+				$tax_amount     = round($total * ($value / 100));
+				$tax_item[$key] = $tax_amount;
+				$grand_total    = $grand_total + $tax_amount;  //add tax val to grand total
+		}
+		
+		$list_tax       = '';
+		foreach($tax_item as $key => $value){ //List all taxes
+			$list_tax .= $key. ' : '. $currency. sprintf("%01.2f", $value).'<br />';
+		}
+		$shipping_cost = ($shipping_cost)?'Shipping Cost : '.$currency. sprintf("%01.2f", $shipping_cost).'<br />':'';
 	}
-	
     ?>
-    </div>
+    <tr>
+        <td colspan="5">
+            <span style="float:right;text-align: right;">
+            <?php echo $shipping_cost. $list_tax; ?>Amount Payable : <?php echo sprintf("%01.2f", $grand_total);?>
+            </span>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="5">
+            <a href="index.php" class="button">Add More Items</a>
+            <button type="submit">Update</button>
+            <a href="paypal-express-checkout" ><img src="images/btn_pay_with_paypal.png" width="179" height="36"></a>
+        </td>
+    </tr>
+  </tbody>
+</table>
+<input type="hidden" name="return_url" value="<?php 
+$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+echo $current_url; ?>" />
+</form>
 </div>
+
 </body>
 </html>
