6ce21a4b1066468242898be1d39201cb1ccc9aee
Sanwebe <saaraan@gmail.com>
2016-01-15 14:25:40 +0530

    Update cart_update.php

--- cart_update.php
+++ cart_update.php
@@ -2,84 +2,60 @@
 session_start();
 include_once("config.php");
 
-//empty cart by distroying current session
-if(isset($_GET["emptycart"]) && $_GET["emptycart"]==1)
+//add product to session or create new one
+if(isset($_POST["type"]) && $_POST["type"]=='add' && $_POST["product_qty"]>0)
 {
-	$return_url = base64_decode($_GET["return_url"]); //return url
-	session_destroy();
-	header('Location:'.$return_url);
+	foreach($_POST as $key => $value){ //add all post vars to new_product array
+		$new_product[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+    }
+	//remove unecessary vars
+	unset($new_product['type']);
+	unset($new_product['return_url']); 
+	
+ 	//we need to get product name and price from database.
+    $statement = $mysqli->prepare("SELECT product_name, price FROM products WHERE product_code=? LIMIT 1");
+    $statement->bind_param('s', $new_product['product_code']);
+    $statement->execute();
+    $statement->bind_result($product_name, $price);
+	
+	while($statement->fetch()){
+		
+		//fetch product name, price from db and add to new_product array
+        $new_product["product_name"] = $product_name; 
+        $new_product["product_price"] = $price;
+        
+        if(isset($_SESSION["cart_products"])){  //if session var already exist
+            if(isset($_SESSION["cart_products"][$new_product['product_code']])) //check item exist in products array
+            {
+                unset($_SESSION["cart_products"][$new_product['product_code']]); //unset old array item
+            }           
+        }
+        $_SESSION["cart_products"][$new_product['product_code']] = $new_product; //update or create product session with new item  
+    } 
 }
 
-//add item in shopping cart
-if(isset($_POST["type"]) && $_POST["type"]=='add')
-{
-	$product_code 	= filter_var($_POST["product_code"], FILTER_SANITIZE_STRING); //product code
-	$product_qty 	= filter_var($_POST["product_qty"], FILTER_SANITIZE_NUMBER_INT); //product code
-	$return_url 	= base64_decode($_POST["return_url"]); //return url
-	
-	//MySqli query - get details of item from db using product code
-	$results = $mysqli->query("SELECT product_name,price FROM products WHERE product_code='$product_code' LIMIT 1");
-	$obj = $results->fetch_object();
-	
-	if ($results) { //we have the product info 
-		
-		//prepare array for the session variable
-		$new_product = array(array('name'=>$obj->product_name, 'code'=>$product_code, 'qty'=>$product_qty, 'price'=>$obj->price));
-		
-		if(isset($_SESSION["products"])) //if we have the session
-		{
-			$found = false; //set found item to false
-			
-			foreach ($_SESSION["products"] as $cart_itm) //loop through session array
-			{
-				if($cart_itm["code"] == $product_code){ //the item exist in array
 
-					$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$product_qty, 'price'=>$cart_itm["price"]);
-					$found = true;
-				}else{
-					//item doesn't exist in the list, just retrive old info and prepare array for session var
-					$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$cart_itm["qty"], 'price'=>$cart_itm["price"]);
-				}
+//update or remove items 
+if(isset($_POST["product_qty"]) || isset($_POST["remove_code"]))
+{
+	//update item quantity in product session
+	if(isset($_POST["product_qty"]) && is_array($_POST["product_qty"])){
+		foreach($_POST["product_qty"] as $key => $value){
+			if(is_numeric($value)){
+				$_SESSION["cart_products"][$key]["product_qty"] = $value;
 			}
-			
-			if($found == false) //we didn't find item in array
-			{
-				//add new user item in array
-				$_SESSION["products"] = array_merge($product, $new_product);
-			}else{
-				//found user item in array list, and increased the quantity
-				$_SESSION["products"] = $product;
-			}
-			
-		}else{
-			//create a new session var if does not exist
-			$_SESSION["products"] = $new_product;
 		}
-		
 	}
-	
-	//redirect back to original page
-	header('Location:'.$return_url);
+	//remove an item from product session
+	if(isset($_POST["remove_code"]) && is_array($_POST["remove_code"])){
+		foreach($_POST["remove_code"] as $key){
+			unset($_SESSION["cart_products"][$key]);
+		}	
+	}
 }
 
-//remove item from shopping cart
-if(isset($_GET["removep"]) && isset($_GET["return_url"]) && isset($_SESSION["products"]))
-{
-	$product_code 	= $_GET["removep"]; //get the product code to remove
-	$return_url 	= base64_decode($_GET["return_url"]); //get return url
+//back to return url
+$return_url = (isset($_POST["return_url"]))?urldecode($_POST["return_url"]):''; //return url
+header('Location:'.$return_url);
 
-	
-	foreach ($_SESSION["products"] as $cart_itm) //loop through session array var
-	{
-		if($cart_itm["code"]!=$product_code){ //item does,t exist in the list
-			$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$cart_itm["qty"], 'price'=>$cart_itm["price"]);
-		}
-		
-		//create a new product list for cart
-		$_SESSION["products"] = $product;
-	}
-	
-	//redirect back to original page
-	header('Location:'.$return_url);
-}
 ?>
